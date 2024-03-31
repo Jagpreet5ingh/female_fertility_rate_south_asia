e12e6529f10fda5769458e710c8aa6ae56416b7f
Jagpreet Singh <jagpreetsingh.singh@mail.utoronto.ca>
2024-03-31 15:45:59 -0400

    update script for data cleaning

--- script/02-data_cleaning.R
+++ script/02-data_cleaning.R
@@ -1,3 +1,12 @@
+#### Preamble ####
+# Purpose : Clean and merging the raw data from unedited_data_fertility_rate.csv and input/data/unedited_data_literacy_rate.csv
+# Author : Jagpreet Singh
+# Date : 31 March, 2024
+# Contact : jagpreetsingh.singh@mail.utoronto.ca
+# License : MIT
+# Prerequisite : install.packages("tidyverse") should be run if not pre-existing
+
+#### Workspace Setup ####
 
 library(tidyverse)
 
@@ -6,15 +15,21 @@ fertility_file <- "input/data/unedited_data_fertility_rate.csv"
 literacy_file <- "input/data/unedited_data_literacy_rate.csv"
 output_file <- "output/data/edited_data.csv"
 
-# Read the CSV files and select two columns
+# Read the CSV files, selecting the Time and respective columns
 fertility_data <- read_csv(fertility_file) %>%
-  select(Time, `South Asia [SAS] - Fertility rate, total (births per woman) [SP.DYN.TFRT.IN]`)
+  filter(Time <= 2020) %>%
+  select(Time, `South Asia [SAS] - Fertility rate, total (births per woman) [SP.DYN.TFRT.IN]`) %>%
+  rename(Year = Time, Fertility_rate = `South Asia [SAS] - Fertility rate, total (births per woman) [SP.DYN.TFRT.IN]`)
 
 literacy_data <- read_csv(literacy_file) %>%
-  select(Time, `South Asia [SAS] - Literacy rate, adult female (% of females ages 15 and above) [SE.ADT.LITR.FE.ZS]`)
+  filter(Time <= 2020) %>%
+  select(Time, `South Asia [SAS] - Literacy rate, adult female (% of females ages 15 and above) [SE.ADT.LITR.FE.ZS]`) %>%
+  rename(Year = Time, Literacy_Rate = `South Asia [SAS] - Literacy rate, adult female (% of females ages 15 and above) [SE.ADT.LITR.FE.ZS]`)
 
-# Combine the selected columns into a single data frame
-selected_data <- bind_cols(fertility_data, literacy_data)
+# Merge the datasets on the "Year" column
+consolidated_data <- merge(fertility_data, literacy_data, by = "Year")
 
-# Write the selected columns to a third file
-write_csv(selected_data, output_file)
+# Write the consolidated data to a third file
+write_csv(consolidated_data, output_file)
+
+
