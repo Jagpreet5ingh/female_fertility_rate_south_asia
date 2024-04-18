b35de438a40c4900f20d0b801179824eb138a64e
Jagpreet Singh <jagpreetsingh.singh@mail.utoronto.ca>
2024-04-17 20:14:33 -0400

    update, run and save output for 02-data_Cleaning.R

--- script/02-data_cleaning.R
+++ script/02-data_cleaning.R
@@ -1,39 +1,38 @@
 #### Preamble ####
-# Purpose : Clean and merging the raw data from unedited_data_fertility_rate.csv and input/data/unedited_data_literacy_rate.csv
+# Purpose : Clean and merging the raw data to gather relevant data for analysis 
 # Author : Jagpreet Singh
-# Date : 31 March, 2024
+# Date : 17 April, 2024
 # Contact : jagpreetsingh.singh@mail.utoronto.ca
 # License : MIT
-# Prerequisite : install.packages("tidyverse") should be run if not pre-existing
+# Prerequisite : Download the global Literacy rate (adult female) and Fertility rate (total) from World Bank Open Data.
 
-#### Workspace Setup ####
 
+#### Workspace setup ####
 library(tidyverse)
-options(warn = -1)
+library(arrow)
 
-# Define file paths
-fertility_file <- "input/data/unedited_data_fertility_rate.csv"
-literacy_file <- "input/data/unedited_data_literacy_rate.csv"
-output_file <- "output/data/edited_data.csv"
+#### Clean data ####
 
-# Suppressing message while reading CSV files
-suppressMessages({
-  # Read the CSV files, selecting the Time and respective columns
-  fertility_data <- read_csv(fertility_file) %>%
-    filter(Time <= 2020) %>%
-    select(Time, `South Asia [SAS] - Fertility rate, total (births per woman) [SP.DYN.TFRT.IN]`) %>%
-    rename(Year = Time, Fertility_rate = `South Asia [SAS] - Fertility rate, total (births per woman) [SP.DYN.TFRT.IN]`)
-  
-  literacy_data <- read_csv(literacy_file) %>%
-    filter(Time <= 2020) %>%
-    select(Time, `South Asia [SAS] - Literacy rate, adult female (% of females ages 15 and above) [SE.ADT.LITR.FE.ZS]`) %>%
-    rename(Year = Time, Literacy_Rate = `South Asia [SAS] - Literacy rate, adult female (% of females ages 15 and above) [SE.ADT.LITR.FE.ZS]`)
-})
+# Read the fertility rate, total data
+fertility_rate_data <- read_csv("data/raw_data/API_SP.DYN.TFRT.IN_DS2_en_csv_v2_43477.csv", skip = 4)
+south_asia_fertility_rate <- fertility_rate_data %>% 
+  filter(`Country Name` == "South Asia") %>% 
+  select(`2000`:`2020`) %>% 
+  pivot_longer(cols = everything(), names_to = "Year", values_to = "fertility_rate_total")
 
-# Merge the datasets on the "Year" column
-consolidated_data <- merge(fertility_data, literacy_data, by = "Year")
+# Read the literacy rate, adult female data
+female_literacy_rate_data <- read_csv("data/raw_data/API_SE.ADT.LITR.FE.ZS_DS2_en_csv_v2_58000.csv", skip = 4)
+south_asia_literacy_rate <- female_literacy_rate_data %>% 
+  filter(`Country Name` == "South Asia") %>% 
+  select(`2000`:`2020`) %>% 
+  pivot_longer(cols = everything(), names_to = "Year", values_to = "female_literacy_Rate")
 
-# Write the consolidated data to a third file
-write_csv(consolidated_data, output_file)
+# Combine the datasets
+combined_data <- left_join(south_asia_fertility_rate, south_asia_literacy_rate, by = "Year")
+
+# Save the combined data to a new CSV file
+write_csv(combined_data, "data/analysis_data/south_asia_2000_2020.csv")
+
+write_parquet(combined_data, "data/analysis_data/south_asia_2000_2020.parquet")
 
 
