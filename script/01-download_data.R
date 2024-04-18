1e2b2850eee2a2882dbaa95e1cc07ac8dc60021e
Jagpreet Singh <jagpreetsingh.singh@mail.utoronto.ca>
2024-04-17 20:25:41 -0400

    updated 01-download_Data.R

--- script/01-download_data.R
+++ script/01-download_data.R
@@ -1,20 +1,13 @@
 #### Preamble ####
-# Purpose: Gather the relevant points, clean up the column names and refine their values
+# Purpose: Instructions to download raw data from the World Bank Open Data
 # Author: Jagpreet Singh
-# Data: 31 March, 2024
+# Data: 17 April, 2024
 # Contact: jagpreetsingh.singh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Access to the World Bank Literacy rate, adult female (% of femaleas ages 15 and above) and Fertility rate, total (births per woman) data.
-
-#### Workspace setup ####
-library(tidyverse)
+# Pre-requisites: None
 
 #### Download data ####
 
-##Data was downloaded from https://data.worldbank.org/indicator/SE.ADT.LITR.FE.ZS?locations=8S&name_desc=true&view=map and https://genderdata.worldbank.org/indicators/sp-dyn-tfrt-in/ from the individual data sets.
-##Data sets for years 2000 - 2020 were downloaded manually.
-
-#### Save data ####
-
-fertility_rate <- read_csv(here::here("input/data/unedited_data_fertility_rate.csv"))
-literacy_rate <- read_csv(here::here("input/data/unedited_data_literacy_rate.csv"))
+# Fertility rate, total (births per woman) dataset was downloaded from: https://data.worldbank.org/indicator/SP.DYN.TFRT.IN
+# Literacy rate, adult female dataset was downloaded from: https://data.worldbank.org/indicator/SE.ADT.LITR.FE.ZS?locations=8S&name_desc=true&view=map
+# Once open the website, find 'download' and click 'csv'
