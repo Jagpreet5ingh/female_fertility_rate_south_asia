0fb893796bfc63f0f926fdffb573d79a26512a38
Jagpreet Singh <jagpreetsingh.singh@mail.utoronto.ca>
2024-04-17 22:25:31 -0400

    upload script 04-model.R

---
+++ script/04-model.R
@@ -0,0 +1,28 @@
+#### Preamble ####
+# Purpose: Simple Linear Regression Model for relationship between female literacy rate and fertility rate in South Asia between 2000-2020
+# Author: Jagpreet Singh
+# Data: 17 April 2024
+# Contact: jagpreetsingh.singh@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 02-data_cleaning.R and run install.packages("modelsummary")
+
+#### Workspace setup ####
+library(tidyverse)
+library(modelsummary)
+
+#### Read data ####
+analysis_data <- read.csv("data/analysis_data/cleaned_data_south_asia_2000_2020.csv")
+
+#### Create model ####
+
+# Fit a linear model
+model <- lm(female_literacy_rate ~ fertility_rate_total, data = analysis_data)
+
+# Summary of the model to see results
+modelsummary::modelsummary(
+  list("Fertility rate" = model),
+  fmt = 2
+)
+
+#### Save model ####
+saveRDS(model, "models/linear_model.rds")
